5283ff1e575a46d4a906a6feb0321497d611b33a
David Arcís Moreno <szikra.dev@gmail.com>
2024-04-01 23:49:55 +0200

    Adds Push lib for retro look and downscaling

--- main.lua
+++ main.lua
@@ -1,27 +1,43 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 -- Initialize the game and its state
 function love.load()
-  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-    fullscreen = false,
-    resizable = false,
-    vsync = true
-  })
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- making the virtual resolution to have a retro look
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+-- Keyboard handling
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
 
 -- Draw the game state to the screen, called after update
 function love.draw()
-  love.graphics.printf(
-    'Hello Pong!', 
-    0, 
-    WINDOW_HEIGHT / 2 - 6,
-    WINDOW_WIDTH, 
-    'center' 
-  )
+    -- begin the virtual resolution drawing
+    push:apply('start')
+
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- end the virtual resolution drawing
+    push:apply('end')
 end
 
 -- Update the game state, called each frame
 -- function love.update(dt)
-  
+
 -- end
