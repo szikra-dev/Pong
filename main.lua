e2bb13338978a23446e4cbcfaa24d67bf01c01aa
David Arcís Moreno <szikra.dev@gmail.com>
2024-04-02 00:13:22 +0200

    Adds items layout

--- main.lua
+++ main.lua
@@ -8,8 +8,11 @@ VIRTUAL_HEIGHT = 243
 
 -- Initialize the game and its state
 function love.load()
-
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- Set the font
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(smallFont)
 
     -- making the virtual resolution to have a retro look
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -31,7 +34,17 @@ function love.draw()
     -- begin the virtual resolution drawing
     push:apply('start')
 
-    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    -- clear the screen with a specific color
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+
+    -- draw welcome text toward the top of the screen
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    -- draw the paddles
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    -- draw the ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     -- end the virtual resolution drawing
     push:apply('end')
