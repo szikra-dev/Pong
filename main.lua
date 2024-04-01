87cd8a52dfb341407ef6597f40bb98b8d1784357
David Arcís Moreno <szikra.dev@gmail.com>
2024-04-02 00:29:01 +0200

    Adds score and paddles movement

--- main.lua
+++ main.lua
@@ -6,12 +6,18 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+-- paddle speed; in update we multiply by the delta time
+PADDLE_SPEED = 200
+
 -- Initialize the game and its state
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    -- Set the font
+    -- Set the fonts
     smallFont = love.graphics.newFont('font.ttf', 8)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+
+    -- Set the active font to smallFont
     love.graphics.setFont(smallFont)
 
     -- making the virtual resolution to have a retro look
@@ -20,6 +26,35 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    -- init score vars
+    player1Score = 0
+    player2Score = 0
+
+    -- paddle positions on the Y axis
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+end
+
+-- Update the game state, called each frame
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then
+        -- add negative paddle speed to current Y scaled by deltaTime
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        -- add positive paddle speed to current Y scaled by deltaTime
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then
+        -- add negative paddle speed to current Y scaled by deltaTime
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        -- add positive paddle speed to current Y scaled by deltaTime
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
 end
 
 -- Keyboard handling
@@ -38,19 +73,20 @@ function love.draw()
     love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
 
     -- draw welcome text toward the top of the screen
+    love.graphics.setFont(smallFont)
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
+    -- draw the score on the left and right center of the screen
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
     -- draw the paddles
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y - 50, 5, 20)
     -- draw the ball
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     -- end the virtual resolution drawing
     push:apply('end')
 end
-
--- Update the game state, called each frame
--- function love.update(dt)
-
--- end
